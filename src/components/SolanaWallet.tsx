1e6bb9afa15a0fa0e890dce6787233471abb0d08
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-08-23 01:35:58 +0530

    private key functions

--- src/components/SolanaWallet.tsx
+++ src/components/SolanaWallet.tsx
@@ -1,27 +1,45 @@
 import { Keypair, PublicKey } from '@solana/web3.js';
 import { mnemonicToSeed } from 'bip39';
 import { derivePath } from 'ed25519-hd-key';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import nacl from "tweetnacl";
+
+interface Wallet {
+    publicKey: PublicKey;
+    privateKey: string;
+    mnemonic: string;
+}
 
 const SolanaWallet = ({ mnemonic }: { mnemonic: string }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [wallets, setWallets] = useState<{ publicKey: PublicKey, mnemonic: string }[]>([]);
+    const [wallets, setWallets] = useState<Wallet[]>([]);
     const [openIndex, setOpenIndex] = useState<number | null>(null);
 
     const toggleAccordion = (index: number) => {
         setOpenIndex(openIndex === index ? null : index);
     };
 
-    const addWallet = () => {
-        const seed = mnemonicToSeed(mnemonic);
+    useEffect(() => {
+        setWallets([]);
+        setCurrentIndex(0);
+    }, [mnemonic]);
+
+    const addWallet = async () => {
+        const seed = await mnemonicToSeed(mnemonic);
         const path = `m/44'/501'/${currentIndex}'/0'`;
         const derivedSeed = derivePath(path, seed.toString()).key;
-        const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
-        const keypair = Keypair.fromSecretKey(secret);
-        setWallets([...wallets, { publicKey: keypair.publicKey, mnemonic }]);
+        const keypair = nacl.sign.keyPair.fromSeed(derivedSeed);
+
+        const publicKey = Keypair.fromSecretKey(keypair.secretKey).publicKey;
+        const privateKey = Buffer.from(keypair.secretKey).toString('hex');
+
+        setWallets([...wallets, { publicKey, privateKey, mnemonic }]);
         setCurrentIndex(currentIndex + 1);
     };
+
+    if (!mnemonic) {
+        return <div></div>;
+    }
 
     return (
         <div className='p-4'>
@@ -70,7 +88,10 @@ const SolanaWallet = ({ mnemonic }: { mnemonic: string }) => {
                         >
                             <div className="p-3">
                                 <div>
-                                    Public Key: {wallet.publicKey.toBase58()}
+                                    <strong>Public Key:</strong> {wallet.publicKey.toBase58()}
+                                </div>
+                                <div>
+                                    <strong>Private Key:</strong> {wallet.privateKey}
                                 </div>
                                 <div className='flex justify-end items-end p-2'>
                                     <button
@@ -79,9 +100,19 @@ const SolanaWallet = ({ mnemonic }: { mnemonic: string }) => {
                                             alert("Public Key copied to clipboard!");
                                         }}
                                         type="button"
-                                        className="items-center text-white bg-purple-700 hover:bg-purple-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700"
+                                        className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700"
                                     >
                                         Copy Public Key
+                                    </button>
+                                    <button
+                                        onClick={() => {
+                                            navigator.clipboard.writeText(wallet.privateKey);
+                                            alert("Private Key copied to clipboard!");
+                                        }}
+                                        type="button"
+                                        className="ml-2 text-white bg-red-700 hover:bg-red-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-red-600 dark:hover:bg-red-700"
+                                    >
+                                        Copy Private Key
                                     </button>
                                 </div>
                             </div>
--- src/components/WalletGenerator.tsx
+++ src/components/WalletGenerator.tsx
@@ -3,6 +3,7 @@ import { generateMnemonic } from 'bip39'
 import { useState } from 'react'
 import SolanaWallet from './SolanaWallet';
 import EthereumWallet from './EthereumWallet';
+import Toast from './Toast';
 
 const WalletGenerator = () => {
     const [mnemonics, setMnemonics] = useState('')
@@ -20,9 +21,7 @@ const WalletGenerator = () => {
     const copyMnemonics = () => {
         navigator.clipboard.writeText(mnemonics).then(() => {
             setIsToastVisible(true);
-            setTimeout(() => {
-                setIsToastVisible(false);
-            }, 3000); // Hide toast after 3 seconds
+            
         });
     };
     return (
@@ -93,24 +92,7 @@ const WalletGenerator = () => {
                 </div>
             }
             {isToastVisible && (
-                <div
-                    id="toast-default"
-                    className="fixed bottom-4 left-1/2 transform -translate-x-1/2 flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
-                    role="alert"
-                >
-                    <div className="ms-3 text-sm font-normal">Mnemonic copied to clipboard!</div>
-                    <button
-                        type="button"
-                        className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-                        onClick={() => setIsToastVisible(false)}
-                        aria-label="Close"
-                    >
-                        <span className="sr-only">Close</span>
-                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                        </svg>
-                    </button>
-                </div>
+                <Toast setIsToastVisible={setIsToastVisible} message='Mnemonic copied to clipboard!' />
             )}
             <div className='p-3 w-full'>
                 <div className=''>
