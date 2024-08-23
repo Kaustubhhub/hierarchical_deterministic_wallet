1ce9fbea3ae62d64ee23d94743b1906412d5487b
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-08-23 12:12:33 +0530

    show balance button

--- src/components/SolanaWallet.tsx
+++ src/components/SolanaWallet.tsx
@@ -114,6 +114,15 @@ const SolanaWallet = ({ mnemonic }: { mnemonic: string }) => {
                                     >
                                         Copy Private Key
                                     </button>
+                                    <button
+                                        onClick={() => {
+                                            
+                                        }}
+                                        type="button"
+                                        className="ml-2 text-white bg-red-700 hover:bg-red-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-red-600 dark:hover:bg-red-700"
+                                    >
+                                        Show balance
+                                    </button>
                                 </div>
                             </div>
                         </div>
