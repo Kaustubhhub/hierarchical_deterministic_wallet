b6e3c22363faf6be63fa1ea6ecc8ce3b1d6499b8
kaustubh <kaustubhkumbhare02@gmail.com>
2025-12-26 08:02:07 +0000

    added airdrop feature

--- src/components/web/solWalletDialog.tsx
+++ src/components/web/solWalletDialog.tsx
@@ -1,23 +1,68 @@
 import type React from "react";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
-import type { Keypair } from "@solana/web3.js";
+import { LAMPORTS_PER_SOL, type Keypair } from "@solana/web3.js";
 import { Button, buttonVariants } from "../ui/button";
 import { CopyIcon } from "lucide-react";
 import { Input } from "../ui/input";
 import { Separator } from "../ui/separator";
 import { toast } from "sonner";
-
+import { useState } from "react";
+import { address, createSolanaRpc, lamports } from "@solana/kit";
 
 function DialogDemo({ children, walletDetail }: { children: React.ReactNode, walletDetail: Keypair }) {
-
+    const SOL_URL = import.meta.env.VITE_SOLANA_DEV_URL;
+    const [balance, setBalance] = useState<string>("****")
     const handleCopy = async () => {
         await navigator.clipboard.writeText(walletDetail.publicKey.toString())
         toast.success("copied to clipboard")
     }
 
-    const showBalance = async() => {
+    const showBalance = async () => {
+        if (!SOL_URL) {
+            alert("URL is not present");
+            return;
+        }
 
-    }
+        const body = {
+            jsonrpc: "2.0",
+            id: 1,
+            method: "getBalance",
+            params: [walletDetail.publicKey.toString()]
+        };
+
+        const result = await fetch(SOL_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body)
+        });
+
+        const data = await result.json();
+
+        if (!data) {
+            toast.error("Failed to fetch balance")
+        }
+
+        setBalance(data.result.value);
+        toast.error("Balanced fetched.")
+    };
+
+    const requestAirdrop = async () => {
+
+        const rpc_url = "https://api.devnet.solana.com";
+        const rpc = createSolanaRpc(rpc_url);
+
+        let receiver = address(walletDetail.publicKey.toString());
+
+        let airdropAmt = lamports(BigInt(1 * LAMPORTS_PER_SOL));
+
+        let signature = await rpc.requestAirdrop(receiver, airdropAmt).send();
+
+        toast.success("Airdrop successfull!")
+
+        console.log(signature);
+    };
+
+
 
     return (
         <Dialog>
@@ -29,6 +74,7 @@ function DialogDemo({ children, walletDetail }: { children: React.ReactNode, wal
                 <DialogHeader>
                     <DialogTitle>Wallet Details</DialogTitle>
                 </DialogHeader>
+                <Button onClick={requestAirdrop} className={`cursor-pointer ${buttonVariants({ variant: "secondary" })}`}>request airdrop</Button>
 
                 <div className="border rounded p-2 flex justify-between items-center transition-all duration-300 hover:border-amber-50">
                     <p className="text-muted-foreground">
@@ -37,7 +83,7 @@ function DialogDemo({ children, walletDetail }: { children: React.ReactNode, wal
                     <Button onClick={handleCopy} className={`cursor-pointer size-10 ${buttonVariants({ variant: "secondary" })}`}><CopyIcon /></Button>
                 </div>
                 <div className="flex justify-between items-center">
-                    <p className="border w-8/12 p-2 rounded">****</p>
+                    <p className="border w-8/12 p-2 rounded">{balance}</p>
                     <Button onClick={showBalance} className={`cursor-pointer ${buttonVariants({ variant: "secondary" })}`}>Show balance</Button>
                 </div>
 
